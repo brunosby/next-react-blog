814644c32495e51f2719fb95b56bc79a5f6d4bb6
Brunosby <brunoshibuya2002@gmail.com>
2026-03-01 17:58:59 -0300

    Add clsx dependency and refactor layout and homepage components
    - Introduced clsx for conditional class names in HomePage.
    - Simplified RootLayout by removing header and footer.
    - Deleted unused About pages and styles.
    - Cleaned up globals.css by removing unnecessary styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import clsx from "clsx";
+
 export default function HomePage() {
-  return <h1>Olá de dentro de page.tsx</h1>;
+  return (
+    <div>
+      <h1
+        className={clsx(
+          "text-6xl font-bold",
+          "text-blue-500",
+          "hover:text-blue-50",
+          "hover:bg-blue-500",
+          "transition",
+          "duration-300",
+        )}
+      >
+        Texto no h1
+      </h1>
+    </div>
+  );
 }
