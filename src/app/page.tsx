c53bca051cf6b1c7b06eab33a36441c433eff8eb
Brunosby <brunoshibuya2002@gmail.com>
2026-03-18 21:25:32 -0300

    Add SpinLoader component for loading indication in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header";
+import SpinLoader from "@/components/SpinLoader";
+import clsx from "clsx";
 
 export default async function HomePage() {
   console.log("HOME");
   return (
     <div>
       <Header />
+      <SpinLoader containerClasses={clsx("min-h-125")} />
     </div>
   );
 }
