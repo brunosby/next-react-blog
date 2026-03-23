509f8557c483166e09e461022cb2915168e6d9f0
Brunosby <brunoshibuya2002@gmail.com>
2026-03-22 22:01:01 -0300

    Add Container component for layout structure in HomePage

---
+++ src/components/Container.index.tsx
@@ -0,0 +1,7 @@
+export function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+      <div className="max-w-5xl mx-auto px-8">{children}</div>
+    </div>
+  );
+}
